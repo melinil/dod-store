c027e52eb11bf958c2d9c9e8dc7834675b2ed90b
Dobrev Doncho (BD/SWD-WDE3) <dod1sf3@bosch.com>
2025-01-21 14:36:46 +0200

    Project files upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component'
+import { DetailsComponent } from './details/details.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignUpComponent
+  },
+  {
+    path: "cart",
+    component: ShoppingCartComponent
+  },
+  {
+    path: "product-details",
+    component: DetailsComponent
+  },
+  {
+    path: "shop",
+    component: ShopPageComponent
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CategoryCardComponent } from './category-card/category-card.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
+import { DeliveryInformationComponent } from './delivery-information/delivery-information.component';
+import { CartCardComponent } from './cart-card/cart-card.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    CardComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailsComponent,
+    ShoppingCartComponent,
+    UserProfileComponent,
+    CategoryCardComponent,
+    ShopPageComponent,
+    DeliveryInformationComponent,
+    CartCardComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatGridListModule,
+    MatTableModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
